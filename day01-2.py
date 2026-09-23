cc4fb7536c667450dc3e47c2089a531936243571
HaechanJ <koddackji123@naver.com>
2026-09-23 12:37:44 +0900

    AI 자동완성 끄기 및 추가 예제

--- day01-2.py
+++ day01-2.py
@@ -1,4 +1,4 @@
-'f-string 실습'
+#f-string 실습
 
 company = "디자인폴더"
 work = "3D스캔+역설계"
---
+++ day01-3.py
@@ -0,0 +1,18 @@
+#문자열 인덱싱
+
+text = "DESIGNFOLDER"
+
+print(text[0])
+print(text[1])
+print(text[2])
+
+company = "DF"
+date = "260923"
+work = "SCAN"
+
+filename =f"{company}_{date}_{work}.pdf"
+
+print(filename[0])
+print(filename[1])
+print(filename[3])
+print(filename[-1])
