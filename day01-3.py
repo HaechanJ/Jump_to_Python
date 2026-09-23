49362cabd1f7550db94cffbff46e4d13a343da16
HaechanJ <koddackji123@naver.com>
2026-09-23 12:42:24 +0900

    Update day01-3.py

--- day01-3.py
+++ day01-3.py
@@ -16,3 +16,13 @@ print(filename[0])
 print(filename[1])
 print(filename[3])
 print(filename[-1])
+
+company = filename[0:2]
+date = filename[3:9]
+work = filename[10:14]
+extention = filename[15:19]
+
+print(company)
+print(date)
+print(work)
+print(extention)
