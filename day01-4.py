8ca3196344943910204de3fdf659f0dafd10cce7
HaechanJ <koddackji123@naver.com>
2026-09-23 12:43:44 +0900

    Create day01-4.py

---
+++ day01-4.py
@@ -0,0 +1,11 @@
+filename = "DF_260923_SCAN.pdf"
+
+company = filename[0:2]
+date = filename[3:9]
+work = filename[10:14]
+extention = filename[15:19]
+
+print(company)
+print(date)
+print(work)
+print(extention)
