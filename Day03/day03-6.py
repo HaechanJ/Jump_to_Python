19e58fe6e313d829445f8086f3b7e854cceb847d
HaechanJ <97072837+HaechanJ@users.noreply.github.com>
2026-09-26 11:31:12 +0900

    d

--- Day03/day03-6.py
+++ Day03/day03-6.py
@@ -1,9 +1,8 @@
 level=12
 has_key=True
 
-if level >=10 and has_key == True:
+if level >=10 and has_key:
     print("보스전 입장")
 else:
     print("입장 불가")
 
-    
---
+++ Day03/day03-7.py
@@ -0,0 +1,7 @@
+gold = 1000
+has_ticket= False
+
+if gold >=500 or has_ticket:
+    print("입장 가능")
+else:
+    print("입장 불가")
